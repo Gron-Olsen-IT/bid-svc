268c8cbd38dd7e37270826a9e340b6f124cf140d
Mads Lajer <eaamalr@students.eaaa.dk>
2023-12-05 10:00:20 +0100

    Added consumption of auction endpoint

--- BidAPI/Services/Infrastructure/InfraRepoRender.cs
+++ BidAPI/Services/Infrastructure/InfraRepoRender.cs
@@ -8,13 +8,10 @@ public class InfraRepoRender : IInfraRepo
 {
     private readonly string INFRA_CONN = Environment.GetEnvironmentVariable("INFRA_CONN") ?? "http://localhost:5000/api/infra/";
     public async Task<HttpStatusCode> UpdateMaxBid(string auctionId, int maxBid)
-    {
-        Console.WriteLine(INFRA_CONN + auctionId);
-        Console.WriteLine("UpdateMaxBid" + auctionId + " " + maxBid);
-        
+    {        
         HttpClient httpClient = new HttpClient();
-        var body = "{\"currentMaxBid\":" + maxBid + "}";
-        var response = await httpClient.PutAsJsonAsync(INFRA_CONN + auctionId, body);
+        
+        var response = await httpClient.PatchAsync($"{INFRA_CONN}/auctions/{auctionId}/?maxBid={maxBid}", null);
         return response.StatusCode;
     }
 }
