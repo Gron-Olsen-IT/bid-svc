e3bca233c84bd04313afb857d00621e7adfb5be2
rmlajer <eeamalr@students.eaaa.com>
2023-12-09 16:18:26 +0100

    Added sidecar and authorize on all endpoints

--- BidAPI/Program.cs
+++ BidAPI/Program.cs
@@ -1,6 +1,14 @@
 using BidAPI.Services;
 using NLog;
 using NLog.Web;
+using sidecar_lib;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.Commons;
+using System.Text;
 
 
 var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
@@ -8,10 +16,11 @@ logger.Debug("init main");
 
 try
 {
+    AuthSidecar sidecar = new(logger);
+
     var builder = WebApplication.CreateBuilder(args);
 
-    // Add services to the container.
-
+    builder.Services.AddSingleton<IVaultClient>(sidecar.vaultClient);
     builder.Services.AddScoped<IBidService, BidService>();
     builder.Services.AddScoped<IBidRepo, BidRepoMongo>();
     builder.Services.AddScoped<IRabbitController, RabbitController>();
@@ -19,6 +28,13 @@ try
     
     builder.Logging.ClearProviders();
     builder.Host.UseNLog();
+
+    builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = sidecar.GetTokenValidationParameters();
+    });
 
     builder.Services.AddControllers();
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -36,6 +52,7 @@ try
 
     app.UseHttpsRedirection();
 
+    app.UseAuthorization();
     app.UseAuthorization();
 
     app.MapControllers();
