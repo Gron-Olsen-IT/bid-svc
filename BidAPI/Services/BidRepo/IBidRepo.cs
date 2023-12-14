6282e0987b637ed9db328ec08285a9d7ab0aacbf
Mads Lajer <eaamalr@students.eaaa.dk>
2023-12-14 12:09:49 +0100

    Added error handling for remaining endpoints

--- BidAPI/Services/BidRepo/IBidRepo.cs
+++ BidAPI/Services/BidRepo/IBidRepo.cs
@@ -4,7 +4,7 @@ using BidAPI.Models;
 namespace BidAPI.Services;
 public interface IBidRepo{
 
-    Task<Bid?> DoesBidExists(string bidId);
+    Task<Bid?> DoesBidExist(string bidId);
     Task<Bid> Post(Bid bid);
     Task<List<Bid>?> GetMaxBids(List<string> auctionIds);
     Task<List<Bid>> Get(string auctionId);
